bfaec39fa399afc5df0cbc63f8a5c2bff93bcb88
Rodrigo Ramos <ramoslozanorodrigoalejandro@gmail.com>
2023-11-09 07:40:56 -0600

    Activitys home, productos y menu lateral

--- app/src/main/java/com/example/prueba_moviles/MainActivity.java
+++ app/src/main/java/com/example/prueba_moviles/MainActivity.java
@@ -5,19 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView registrarCuenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registrarCuenta = findViewById(R.id.jlbNewAccount);
+
+
+        registrarCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_register.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    // Dentro de la clase ActivityA
-    public void irAActivityB(View view) {
+    //Metodo para pasar de una Activity a otra
+    public void PasarActivity(View view) {
         Intent intent = new Intent(this, MenuLateralActivity.class);
         startActivity(intent);
     }
 
+
 }
+
--- app/src/main/java/com/example/prueba_moviles/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/prueba_moviles/ui/gallery/GalleryFragment.java
@@ -4,28 +4,61 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.prueba_moviles.R;
 import com.example.prueba_moviles.databinding.FragmentGalleryBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Agregar el código del Spinner aquí
+        Spinner spinner = root.findViewById(R.id.spTipo);
+
+        // Crear una lista de elementos para el Spinner
+        List<String> listaElementos = new ArrayList<>();
+        listaElementos.add("Aceites");
+        listaElementos.add("Autopartes");
+
+
+        // Crear un adaptador
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, listaElementos);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // Manejar la selección del Spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String elementoSeleccionado = listaElementos.get(position);
+                // Realiza acciones con el elemento seleccionado
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Maneja la selección nula si es necesario
+            }
+        });
+
         return root;
     }
 
